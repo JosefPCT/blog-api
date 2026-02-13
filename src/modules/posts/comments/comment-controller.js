5fafa957e2596d1c65e274f92ff25b53008fb0c4
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-02-13 16:55:52 +0800

    chore: update handler of comments to include data of postId and commentId in logging

--- src/modules/posts/comments/comment-controller.js
+++ src/modules/posts/comments/comment-controller.js
@@ -4,34 +4,39 @@ const commentsService = require('./comment-service.js');
 module.exports.createPostComment = [
   async(req, res) => {
     console.log('/posts/:postId/comments POST route handler');
-    res.status(200).json('POST');
+    console.log(`:postId is ${req.params.postId}`);
+    res.status(200).json(`POST a comment, :postId is ${req.params.postId}`);
   }
 ]
 
 module.exports.getPostComments = [
   async(req, res) => {
     console.log('`/posts/:postId/comments GET route handler');
-    res.status(200).json('GET all comments');
+    console.log(`:postId is ${req.params.postId}`);
+    res.status(200).json(`GET all comments, :postId is ${req.params.postId}`);
   }
 ]
 
 module.exports.getSpecificPostComment = [
   async(req, res) => {
     console.log(`'/posts/:postId/comments/:commentId' GET route handler`);
-    res.status(200).json('GET a specific comment');
+    console.log(`:postId is ${req.params.postId}, :commentId is ${req.params.commentId}`);
+    res.status(200).json(`GET a specific comment, :postId is ${req.params.postId}, :commentId is ${req.params.commentId}`);
   }
 ]
 
 module.exports.updateSpecificPostComment = [
   async(req, res) => {
     console.log(`'/posts/:postId/comments/:commentId' PUT/PATCH route handler`);
-    res.status(200).json('UPDATE a specific commment');
+    console.log(`:postId is ${req.params.postId}, :commentId is ${req.params.commentId}`);
+    res.status(200).json(`UPDATE a specific comment, :postId is ${req.params.postId}, :commentId is ${req.params.commentId}`);
   }
 ]
 
 module.exports.deleteSpecificPostComment = [
   async(req, res) => {
     console.log(`'/posts/:postId/comments/:commentId' DELETE route handler`);
-    res.status(200).json('DELETE a specific comment');
+    console.log(`:postId is ${req.params.postId}, :commentId is ${req.params.commentId}`);
+    res.status(200).json(`DELETE a specific comment, :postId is ${req.params.postId}, :commentId is ${req.params.commentId}`);
   }
 ]
