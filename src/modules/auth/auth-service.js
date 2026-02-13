0bc4684bc0273387361c6abdf73263fd312e9b04
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-02-13 08:39:03 +0800

    fix: remove try...catch block in the service layer of the auth module

--- src/modules/auth/auth-service.js
+++ src/modules/auth/auth-service.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
+
 const jwt = require("jsonwebtoken");
+
+const customErrorType = require('../../utils/extended-errors.js');
 
 // Creates a token using `jwt.sign`, that takes in three things:
 // A payload (usually user information to help validate the user i.e id),
@@ -9,14 +12,14 @@ const jwt = require("jsonwebtoken");
 // Then send user information with the token to the client
 // TODO: might need to remove some sensitive information on the user object (i.e password/hash) 
 module.exports.loginWithJwt = (id, email) => {
-  try {
-    const token = jwt.sign({ sub: id, email: email }, process.env.JWT_SECRET, {
-      algorithm: "HS256",
-      expiresIn: "1h",
-    });
-    return token;
-  } catch (error) {
-    console.error("Error in authService.loginWithJwt");
-    throw error;
+  const token = jwt.sign({ sub: id, email: email }, process.env.JWT_SECRET, {
+    algorithm: "HS256",
+    expiresIn: "1h",
+  });
+
+  if(!token){
+    throw new customErrorType.BadRequest(`Token not created succesfully`);
   }
+
+  return token;
 };
