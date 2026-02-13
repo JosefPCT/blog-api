eabfa1a4b3c9840b017bdc0d8c4d45d18e61bdcd
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-02-13 16:20:12 +0800

    fix: change post controller handler names to more apt names, and point the actual route to their controller handler

--- src/modules/posts/post-controller.js
+++ src/modules/posts/post-controller.js
@@ -1,2 +1,41 @@
 const postsService = require('./post-service.js');
 
+// Handler for POST `/posts` route
+module.exports.createPost = [
+  async(req, res) => {
+    console.log('`/posts` POST route handler');
+    res.status(200).json('POST');
+  }
+]
+
+// Handler for GET `/posts` route
+module.exports.getAllPosts = [
+  async(req, res) => {
+    console.log(`'/posts' GET route handler`);
+    res.status(200).json('GET');
+  }
+]
+
+// Handler for GET `/posts/:id` route
+module.exports.getPostById = [
+  async(req, res) => {
+    console.log(`'/posts/:id' GET route handler`);
+    res.status(200).json('GET');
+  }
+]
+
+// Handler for PUT `/posts/:id` route
+module.exports.updatePostById = [
+  async(req, res) => {
+    console.log(`'/posts/:id' PUT route handler`);
+    res.status(200).json('PUT');
+  }
+]
+
+// Handler for DELETE `/posts/:id` route
+module.exports.deletePostById = [
+  async(req, res) => {
+    console.log(`'/posts/:id' DELETE route handler`);
+    res.status(200).json('DELETE');
+  }
+]
--- src/modules/posts/post-route.js
+++ src/modules/posts/post-route.js
@@ -4,6 +4,10 @@ const router = Router();
 
 const controller = require('./post-controller.js');
 
-router.get;
+router.post('/', controller.createPost);
+router.get('/', controller.getPosts);
+router.get('/:id', controller.getPostById);
+router.put('/:id', controller.updatePostById);
+router.delete('/:id', controller.deletePostById);
 
 module.exports = router;
