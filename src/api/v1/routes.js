63a8608e2dee285f5d076870d02ea5523f69327b
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-02-13 16:47:46 +0800

    fix: create the routing of nested route of '/posts/:postId/comments/:commentsId' on the main router

--- src/api/v1/routes.js
+++ src/api/v1/routes.js
@@ -4,9 +4,11 @@ const apiRouter = Router();
 const usersRoutes = require('../../modules/users/user-route.js');
 const authRoutes = require('../../modules/auth/auth-route.js');
 const postRoutes = require('../../modules/posts/post-route.js');
+const commentRoutes = require('../../modules/posts/comments/comment-route.js');
 
 apiRouter.use('/auth', authRoutes);
 apiRouter.use('/users', usersRoutes);
 apiRouter.use('/posts', postRoutes);
+postRoutes.use('/:postId/comments', commentRoutes);
 
 module.exports = apiRouter;
