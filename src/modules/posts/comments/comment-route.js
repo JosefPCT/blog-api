fa71782ef040ed95630e6772c358abecbd22bd11
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-02-13 16:45:36 +0800

    chore: point the actual comment routes to their controller handlers

--- src/modules/posts/comments/comment-route.js
+++ src/modules/posts/comments/comment-route.js
@@ -1,4 +1,15 @@
 const { Router } = require('express');
+const router = Router({ mergeParams: true});
 
-const router = Router();
+const controller = require('./comment-controller.js');
 
+router.post('/', controller.createPostComment);
+
+router.get('/', controller.getPostComments);
+router.get('/:commentId', controller.getSpecificPostComment);
+
+router.put('/:commentId', controller.updateSpecificPostComment);
+
+router.delete('/:commentId', controller.deleteSpecificPostComment);
+
+module.exports = router;
