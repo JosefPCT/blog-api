676959df47fcd0cca1f96c83921ab90af49665f7
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-03-23 11:38:06 +0800

    feat: implement mode search, (AND, NOT, OR) using req.query to GET '/posts', implement case insensitive searches with title and text fields

--- src/modules/posts/post-queries.js
+++ src/modules/posts/post-queries.js
@@ -15,7 +15,7 @@ module.exports.createPostByUserId = async(data, userId) => {
 
 // Find all posts
 // Optional arguments from the req.query string `optionalArgs` : title, text, isPublished, dateFrom, dateTo, authorFirstName, authorLastName, numberOfComments, page, mode
-// Optional `sortArgs` when `sort` req.query string is present
+// Optional `sortArgs` when `sort` req.query string is present, `sortArgs` can have key values of:  title, text, isPublished, createdAt, updatedAt, comments
 module.exports.findAllPosts = async(optionalArgs, sortArgs) => {
 
   let authorIds = [];
@@ -34,7 +34,6 @@ module.exports.findAllPosts = async(optionalArgs, sortArgs) => {
     authorIds = targetUsers.map(user => user.id);
   }
   
-
   if(optionalArgs.numberOfComments && parseInt(optionalArgs.numberOfComments) !== 0){
     const usersWithCommentCount = await prisma.comment.groupBy({
       by: ['postId'],
@@ -47,24 +46,29 @@ module.exports.findAllPosts = async(optionalArgs, sortArgs) => {
     postIds = filteredUsers.map(comment => comment.postId);
   }
 
-  // const userIds = [...authorIds, ...userIdsForComments];
+  // const userIds = [...authorIds, ...postIds];
 
   const whereData = {
-    title: optionalArgs.title ? { contains: optionalArgs.title } : undefined,
-    text: optionalArgs.text ? { contains: optionalArgs.text } : undefined,
+    title: optionalArgs.title ? { contains: optionalArgs.title, mode: 'insensitive' } : undefined,
+    text: optionalArgs.text ? { contains: optionalArgs.text, mode: 'insensitive' } : undefined,
     isPublished: optionalArgs.isPublished ? ( optionalArgs.isPublished === 'true' ? true : false ) : undefined,
     createdAt: optionalArgs.dateFrom || optionalArgs.dateTo ? { gte: createdDateFrom, lte: createdDateTo} : undefined,
     authorId: optionalArgs.authorFirstName || optionalArgs.authorLastName ? { in: authorIds } : undefined,
     id: (optionalArgs.numberOfComments && parseInt(optionalArgs.numberOfComments) !== 0) ? { in: postIds} : undefined,
     comments: optionalArgs.numberOfComments && parseInt(optionalArgs.numberOfComments) === 0 ? { none: {}} : undefined,
   };
-
   console.log(whereData);
+
+  const OrWhereData = Array.from(Object.keys(whereData), (key) => ({ [key]: whereData[key]}));
 
   return await prisma.post.findMany({
     take: optionalArgs.page ? 2 : undefined,
     skip: optionalArgs.page ? (optionalArgs.page - 1 ) * 2 : undefined,
-    where: whereData,
+    where: {
+      AND: optionalArgs.mode === 'and' || optionalArgs.mode === undefined ? whereData : undefined,
+      NOT: optionalArgs.mode === 'not' ? { OR: OrWhereData } : undefined,
+      OR: optionalArgs.mode === 'or' ? OrWhereData : undefined,
+    },
     orderBy: {
       title: sortArgs.title ? sortArgs.title : undefined,
       text: sortArgs.text ? sortArgs.text : undefined,
