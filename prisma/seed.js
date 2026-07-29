132e699447144360558399677424e4bd023295a5
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-07-29 14:29:54 +0800

    feat: add seed file and configure package.json to find the seed and change build script to run the seed file

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,14 +1,23 @@
 // const prisma = require("../../config/prisma.js");
 const prisma = require("../src/config/prisma.js");
+// const passwordUtils = require('../../utils/passwordUtils.js');
+const passwordUtils = require('../src/utils/passwordUtils.js')
 
 async function main(){
+
+  const adminPassword = "admin"
+  const hashedAdminPassword = passwordUtils.generatePassword(adminPassword);
+
   const dani = await prisma.user.upsert({
     where: { email: 'dani@gmail.com' },
     update: {},
     create:{
       email: 'dani@gmail.com',
       firstName: 'Dani',
-      lastName: 'Robles'
+      lastName: 'Robles',
+      hash: hashedAdminPassword,
+      isAuthor: true,
+      isAdmin: true
     }
   })
 }
